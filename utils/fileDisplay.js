569d12e92f928c5bef85dd44e6bf2ddc93fbf52f
xumingquan <xumingquan@yidian-inc.com>
2018-08-16 17:18:34 +0800

    新增注释

--- utils/fileDisplay.js
+++ utils/fileDisplay.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
 const path = require('path');
+/**
+ * @description: 所有压缩文件 
+ * @param (String) filePath: 文件路径
+ * @return (Array ) ：User/docs/filename.xml 
+ */
 module.exports = function(filePath){
   //根据文件路径读取文件，返回文件列表
   var result = fs.readdirSync(filePath);
